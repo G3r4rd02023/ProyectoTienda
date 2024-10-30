29876ded70e8318cc449c788dac7c0100d5cfb2b
Gerardo Lanza <glanza007@gmail.com>
2024-10-30 12:42:49 -0600

    Project Basic Creation

---
+++ Ecommerce/Ecommerce.Frontend/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ecommerce.Frontend.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
